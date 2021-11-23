4aa83db074bd79483d231e034346d8daf50bd6af
= <=>
2021-11-23 19:28:04 +0700

    comment

--- Tries/Tries.h
+++ Tries/Tries.h
@@ -7,12 +7,12 @@
 using namespace std;
 
 
-<<<<<<< HEAD
+
 // Xây dựng cấu trúc Tries
-=======
+
 
 const int soKiTu = 26; //so luong ki tu trong bang chu cai
->>>>>>> 9ee0a3a58d4d2ee5361f4bd2ac9287a86c46716a
+
 struct trieNode
 {
 	trieNode* next[soKiTu]; // tro den cac node tuong ung voi ki tu tu a den z
--- Tries/main.cpp
+++ Tries/main.cpp
@@ -9,14 +9,13 @@ void main()
 	getline(cin, s);
 
 	trieNode* root = creatTrieFromFile("Dic.txt");
-<<<<<<< HEAD
 
 	int* a = new int[26];
 	for (int i = 0; i < 26; i++)
-=======
+
 	int* a = new int[soKiTu]; // mang chua so lan xuat hien cua cac ki tu
 	for (int i = 0; i < soKiTu; i++) // Khoi tao phan tu mang =0
->>>>>>> 9ee0a3a58d4d2ee5361f4bd2ac9287a86c46716a
+
 	{
 		a[i] = 0;
 	}
@@ -25,14 +24,11 @@ void main()
 	{
 		a[s[i] - 'a'] += 1;
 	}
-<<<<<<< HEAD
 
-	queue<string> dsTu;
-	string word = "";
-=======
+
 	queue<string> dsTu; // Hang doi de luu nhung tu tao duoc
 	string word = ""; // chua cac ki tu
->>>>>>> 9ee0a3a58d4d2ee5361f4bd2ac9287a86c46716a
+
 	findWord(root, a, 0, dsTu, word);
 	// xuat ket qua ra man hinh
 	cout << dsTu.size()<<endl;
